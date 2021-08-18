58fdde245ed0a794228bf2332eee64e6d50ba79e
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-08-18 23:53:59 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import telebot
-import os
-TOKEN = os.getenv('TOKEN')
+# import os
+# TOKEN = os.getenv('TOKEN')
+TOKEN = "1937014541:AAEAMxaXzB0ZUmYJdzJ-0W25gPNnH50WFw4"
+
 bot = telebot.TeleBot(TOKEN)
+print('started')
+@bot.message_handler(commands=['start'])
+def send_welcome(message):	
+	rm = telebot.types.ReplyKeyboardMarkup()
+	rm.add("Im good","im not ok")
+	print("re:",rm.to_json())
+	print(message.text)
+	bot.reply_to(message,text=f"Hi,{message.from_user.first_name} How are you?",reply_markup= rm)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=['start'])
+def send_welcome(message):	
+	rm = telebot.types.ReplyKeyboardMarkup()
+	rm.add("Im good","im not ok")
+	print("re:",rm.to_json())
+	print(message.text)
+	bot.reply_to(message,text=f"Hi,{message.from_user.first_name} How are you?",reply_markup= rm)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-    
 bot.polling()
