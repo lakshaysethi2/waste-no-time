1e6e5f8b8451fec3dfabb98e17fa4309d07ef552
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-09-15 00:28:51 +0000

    this is so hard to do so im using peewee

--- keyvalue.py
+++ keyvalue.py
@@ -3,6 +3,7 @@ from sqlite3 import Error
 # con = sqlite3.connect('keyValPair.db')
 database = r"./keyValPair.db"
 
+
 def create_connection(db_file):
     
     conn = None
@@ -12,31 +13,24 @@ def create_connection(db_file):
         print(e)
 
     return conn
+conn = create_connection(database)
+cur = conn.cursor()
 
-
-def select_all_key_values(conn):
- 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Ar_Activity")
+def select_all_key_values():
+    cur.execute("SELECT * FROM keyvalue")
 
     rows = cur.fetchall()
 
     newList =[]
     for index,row in enumerate(rows):
-        if '2021-08-20' in row[9] and row[6] is not None:
-            newList.append({"name":row[2],"starttime":row[9],"notes":row[13]})
-    newList.sort(key=lambda x: x['starttime'], reverse=False)
+        newList.append((row[1],row[2]))
         
-    print("name","starttime","notes")
-    for x in newList:
-        if 'None' not in str(x['notes']):
-            n = str(x['notes']).replace('\n', '')
-            notes = f" n: { n }"
-        else:
-            notes = ""
-        print(x["starttime"],x["name"],notes )
-
-
+    
+    text = str(("key","value"))
+    for tupple in newList:
+        text += str(tupple)
+    print(text)
+    return text
 
 
 
@@ -44,7 +38,7 @@ def main():
     
 
     # create a database connection
-    conn = create_connection(database)
+    
     with conn:
         # print("1. Query task by priority:")
         # select_task_by_priority(conn, 1)
@@ -59,8 +53,6 @@ def main():
 
 
 def create_table_in_DB():
-    conn = create_connection(database)
-    cur = conn.cursor()
     cur.execute("""CREATE TABLE keyvalue (
                     id INT PRIMARY KEY,
                     key VARCHAR NOT NULL,
@@ -69,7 +61,34 @@ def create_table_in_DB():
     )
 
 
-try:
-    create_table_in_DB()
-except Exception as e:
-    print(e)
+
+
+def add_to_db(key,value):
+    cur.execute("SELECT * FROM keyvalue")
+    rows = cur.fetchall()
+
+    for row in rows:
+        if key  == row[1]:
+            cur.execute(f""" INSERT INTO keyvalue(key,value)
+                    VALUES({key},{value});
+    """)
+
+    cur.execute(f""" INSERT INTO keyvalue(key,value)
+                    VALUES({key},{value});
+    """)
+    cur.execute("COMMIT")
+
+
+
+
+
+# try:
+# create_table_in_DB()
+key = '"who Am I?"'
+value = '"Ni shi Programmer!"'
+add_to_db(key,value)
+select_all_key_values()
+
+# except Exception as e:
+#     print(str(e)+"\n\n\n\n\n")
+    
