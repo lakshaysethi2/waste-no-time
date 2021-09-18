bac77eaadb0e15f22d001b976d9a9a155b4fa49f
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-09-18 14:36:48 +1200

    commednted

--- goals.py
+++ goals.py
@@ -1,19 +1,19 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-#https://firebase.google.com/docs/firestore/quickstart#python_7
-# Use a service account
-cred = credentials.Certificate('/home/ls/Desktop/code/GitHub/telegrambot-learning/sa.json')
-firebase_admin.initialize_app(cred)
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
+# #https://firebase.google.com/docs/firestore/quickstart#python_7
+# # Use a service account
+# cred = credentials.Certificate('/home/ls/Desktop/code/GitHub/telegrambot-learning/sa.json')
+# firebase_admin.initialize_app(cred)
 
-firebase_db = firestore.client()
+# firebase_db = firestore.client()
 
 
 def get_goals_list():
     return 'eat'
 
-collectionRef = firebase_db.collection('users')#collectionreference
+# collectionRef = firebase_db.collection('users')#collectionreference
     
-all_users = collectionRef.stream()
+# all_users = collectionRef.stream()
 
-print(all_users)
+# print(all_users)
