d4c9f68ed1ba5bed5a7066d649afe3ff8924f7bd
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-10-20 12:40:38 +1300

    Create assistant.py

---
+++ assistant.py
@@ -0,0 +1,21 @@
+
+
+def take_care_of_basics():
+    last_eats = ""
+    last_sleeps = ""
+    text = f"""Hi you need to take care of basics!
+    are you feeling tired? if so do you know why?
+    have you slept enough?
+    {last_sleeps}
+    have you eaten enough and healthy?
+    {last_eats}
+    do you need to shower?
+    are you feeling sick?
+    have you brushed your teeth?
+    do you need to change your clothes?
+    do you need to do the laundary?
+    is your room/car clean?
+    do you want to talk to jiyoon?
+    how about going for a walk?
+    
+    """
