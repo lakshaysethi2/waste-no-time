e5f7d569c1bbbef53ca05b415ab27fa6f155f9a5
Lakshay Sethi <45325278+lakshaysethi@users.noreply.github.com>
2021-11-04 18:15:20 +1300

    calenda

--- calendarfile.py
+++ calendarfile.py
@@ -1,5 +1,23 @@
 from manictime import getLastfewHours
-
+from types import SimpleNamespace
 def get_calendar_html():
-    text = getLastfewHours(True)    
-    
+
+    html = ''
+
+
+    text = getLastfewHours(False) 
+    # this text we need to parse and write an html file 
+    lines = text.split('\n')
+    activities = []
+    for line in lines:
+        if 'from - to - activity' not in line:
+            act = SimpleNamespace()
+            act.from_time = line.split(' - ')[0]
+            act.to_time = line.split(' - ')[2]
+            act.duration = line.split(' - ')[3]
+            act.tag = line.split(' - ')[4]
+            
+
+           
+
+
