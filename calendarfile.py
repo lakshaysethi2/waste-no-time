cf24ca722cf9a38cbdcf1c574c70a0cc614ad12d
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-12-04 14:19:02 +1300

    Update calendarfile.py

--- calendarfile.py
+++ calendarfile.py
@@ -10,6 +10,7 @@ def getStyle(tag:str):
      'writing' : '#72dba1',
      'goal setting' : '#72dba1',
      'cleaning' : '#72dba1',
+     'linux' : '#72dba1',
      'reading' : '#72dba1',
      'udemy' : '#72dba1',
      'job apply' : '#72dba1',
