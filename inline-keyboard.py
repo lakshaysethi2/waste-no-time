20064745ea6f11ed84a88ed77abd77dac3ef2bbc
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2022-01-22 22:54:34 +1300

    Update inline-keyboard.py

--- inline-keyboard.py
+++ inline-keyboard.py
@@ -1,17 +1,18 @@
-# https://api.telegram.org/1909326610:AAFh7rsp1dbD6XJ2IGmn5Og8ZfIuF6ZRmNk/getupdates?timeout=100
+# https://api.telegram.org/bot5061167346:AAECJdb_-U9jQorMiJRsITRJBRyf-53Ctv4/getupdates?timeout=100
 import requests
+import json
 
-base_url = "https://api.telegram.org/1909326610:AAFh7rsp1dbD6XJ2IGmn5Og8ZfIuF6ZRmNk"
+base_url = "https://api.telegram.org/bot5061167346:AAECJdb_-U9jQorMiJRsITRJBRyf-53Ctv4"
 LAKSHAY_CID= 1040271347
-text = """please select a goal to work on from the following
-1 be productive
-2 fix CTKCYB-5600
-3 fix CTKCYB-5601
-4 Improve goals-cli"""
-reply_markup = '''{"inline_keyboard":[[{"text":"1","url":"http://t.co/"},{"text":"2","url":"http://t.co/"},
-{"text":"3","url":"http://t.co/"}
-]]}'''
-
+text = """select your energy level"""
+reply_markup = json.loads('{"inline_keyboard":[[]]}')
+ik = reply_markup.get('inline_keyboard')
+ik[0].append({'text': 'very low', 'url': 'idk.lak.nz'})
+ik[0].append({'text': 'low', 'url': 'idk.lak.nz'})
+ik[0].append({'text': 'low-med', 'url': 'idk.lak.nz'})
+ik[0].append({'text': 'high-med', 'url': 'idk.lak.nz'})
+ik[0].append({'text': 'high', 'callback_data': 'idk.lak.nz'})
+reply_markup = json.dumps(reply_markup)
 url = f"{base_url}/sendmessage?text={text}&chat_id={LAKSHAY_CID}&reply_markup={reply_markup}"
 r = requests.get(url)
 
