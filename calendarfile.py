3dcf0663202a2485bbb916ac13538feec8bc1212
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2022-01-22 21:33:12 +1300

    removed layer on top

--- calendarfile.py
+++ calendarfile.py
@@ -67,16 +67,14 @@ def getHeight(dur_string):
 
 
 def getHtml(activities):
-    act_div = "<div class='col-4 layer-on-top-container'> \
-            <div class='layer-on-top'></div>"
+    act_div = "<div class='col-4'>"
     for act in activities:
         name = act.tag
         height = getHeight(act.duration)
         duration = act.duration
         dur_int = int(duration.split(":")[0])
         if name == "sleep" and dur_int > 2  : act_div += f"</div> \
-            <div class='col-4 layer-on-top-container'> \
-            <div class='layer-on-top'> </div>"
+            <div class='col-4'>"
 
         act_div += f'''<div style="height:{height}px; {act.style}" class = "activity-aware act-1"> 
                             {act.from_time} - {name}<br>   
