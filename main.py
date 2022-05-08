60ab266ee1d580a167a595f03c6d63dfa1744a69
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2022-05-09 07:25:11 +1200

    Update main.py

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ def keyvalue(message):
 		except KeyError:
 			text = 'not found'
 	rm.__init__()
-	rm.add('thanks')
+	rm.add('/check')
 	for key in database.keys():
 		rm.add('/key '+str(key))
 	bot.send_message(LAKSHAY_CID,text=text,reply_markup=rm)
