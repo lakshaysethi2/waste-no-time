3131a3026c52f446b4802f8e160cd33177099e78
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2022-06-05 08:17:13 +1200

    added  writing to require notes (#75)

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from assistant import *
 from calendarfile import get_calendar_html
 
 # keep tags in this array in lower case
-array_of_tags_for_which_notes_are_required = ['plantme','fliss', 'trying or setting up','doing phone','food']
+array_of_tags_for_which_notes_are_required = ['plantme','fliss', 'trying or setting up','doing phone','food',
+'writing',
+]
 CHECKINTERVAL=30
 activities_markup = [
 	'/now driving',
