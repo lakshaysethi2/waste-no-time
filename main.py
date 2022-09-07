07c85f758559262861084dc9a9f95a8320837ff6
Lakshay Sethi <8218018-lakshaynz@users.noreply.gitlab.com>
2022-09-07 20:46:42 +0000

    add writing and shopping to require notes

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ import random
 
 # keep tags in this array in lower case
 array_of_tags_for_which_notes_are_required = ['plantme','fliss', 'trying or setting up','doing phone','food',
-#'writing',
+'writing',
+'shopping',
 'pbtech',
 'programming',
 'linux',
