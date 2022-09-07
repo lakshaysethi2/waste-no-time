0f4cc9aac3f5c96872a3d35241fc265509a9a729
Lakshay Sethi <8218018-lakshaynz@users.noreply.gitlab.com>
2022-09-07 11:19:03 +0000

    added some required notes

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ import random
 # keep tags in this array in lower case
 array_of_tags_for_which_notes_are_required = ['plantme','fliss', 'trying or setting up','doing phone','food',
 #'writing',
+'pbtech',
+'programming',
+'linux',
 ]
 CHECKINTERVAL=30
 activities_markup = [
