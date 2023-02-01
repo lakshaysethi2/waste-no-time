c77ce26b10e0f396dc9566aa3feb19bcea1f5fd2
Lakshay Sethi <8218018-lakshaynz@users.noreply.gitlab.com>
2023-02-01 03:15:33 +0000

    updated variables

--- main.py
+++ main.py
@@ -26,49 +26,7 @@ array_of_tags_for_which_notes_are_required = ['plantme','fliss', 'trying or sett
 ]
 PRODUCTION=os.environ.get('PRODUCTION')
 
-the_activities_markup = [
-	'/key ci, 600',
-	'/key ci, 120',
-	'/key ci, 1200',
-	'/key ci, 20',
-	'/now reading',
-	'/now job',
-	'/now writing',
-	'/now cleaning',
-	'/now walking',
-	'/now exercise',
-	'/now driving',
-	'/now uber',
-	'/now programming',
-	'/now linux',
-	'/now doing phone',
-	'/now goal setting',
-	"/now sleep",
-	"/now eye candy",
-	"/now food",
-	'/now Trying or setting up',
-	'/now other people',
-	'/now plantme',
-	'/now udemy',
-	'/top 24',
-	'/top 24*7',
-	'/top 24*30',
-	'/top 24*90',
-	'/now bio',
-	'/now manictime',
-	'/now thinking',
-	'/now family',
-	'/now ecl',
-	'/now YouTube',
-	'/now shopping',
-	'/now data',
-	'/now money',
-	'/now sick',
-	'/unlockdash',
-	'/lockdash',
-
-
-]
+from variables import the_activities_markup
 import os
 TOKEN = os.getenv('TELEGRAM_BOT_API_KEY')
 bot = telebot.TeleBot(TOKEN)
